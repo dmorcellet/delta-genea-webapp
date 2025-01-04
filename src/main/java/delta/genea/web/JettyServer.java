0c374068eb17dd3bc046e7b98188f3d47e426fbf
Damien Morcellet <dmorcellet@free.fr>
2025-01-04 18:12:31 +0100

    Allow runtime as a stand-alone, packaged app

--- src/main/java/delta/genea/web/JettyServer.java
+++ src/main/java/delta/genea/web/JettyServer.java
@@ -1,11 +1,15 @@
 package delta.genea.web;
 
+import java.net.URL;
+
 import org.eclipse.jetty.server.Connector;
 import org.eclipse.jetty.server.Server;
 import org.eclipse.jetty.server.ServerConnector;
-import org.eclipse.jetty.servlet.DefaultServlet;
+import org.eclipse.jetty.server.handler.HandlerList;
+import org.eclipse.jetty.server.handler.ResourceHandler;
 import org.eclipse.jetty.servlet.ServletContextHandler;
 import org.eclipse.jetty.servlet.ServletHolder;
+import org.eclipse.jetty.util.resource.Resource;
 
 /**
  * Jetty server for the genea application.
@@ -27,17 +31,25 @@ public class JettyServer
     server.setConnectors(new Connector[] {connector});
     ServletContextHandler context=new ServletContextHandler(ServletContextHandler.SESSIONS);
     context.setContextPath("/");
-    server.setHandler(context);
     // Dynamic contents
     ServletHolder genea=new ServletHolder("genea",GeneaServlet.class);
     context.addServlet(genea,"/genea");
     // Static contents
-    ServletHolder holderHome=new ServletHolder("static-home",DefaultServlet.class);
-    holderHome.setInitParameter("resourceBase","ressources");
-    holderHome.setInitParameter("dirAllowed","true");
-    holderHome.setInitParameter("pathInfoOnly","true");
-    context.addServlet(holderHome,"/ressources/*");
+    ResourceHandler resHandler = new ResourceHandler();
 
+    ClassLoader cl=this.getClass().getClassLoader();
+    URL url = cl.getResource("index.html");
+    System.out.println("URL: "+url);
+    String dirUrlStr = url.toString().replace("index.html","");
+    URL dir = new URL(dirUrlStr);
+    System.out.println("URL: "+dir);
+    Resource ressource = Resource.newResource(dir);
+    resHandler.setBaseResource(ressource);
+
+    HandlerList handlers=new HandlerList();
+    handlers.addHandler(resHandler);
+    handlers.addHandler(context);
+    server.setHandler(handlers);
     // Start server!
     server.start();
   }
